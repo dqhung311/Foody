d33edd73fc499bc3cb16fef01e627823ff64eb24
Dao Quang Hung <daoquanghung@Daos-MacBook-Pro.local>
2017-05-23 10:34:57 +0700

    init

---
+++ Foody/TableViewCell1.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell1.swift
+//  Foody
+//
+//  Created by Dao Quang Hung on 5/23/17.
+//  Copyright © 2017 Dao Quang Hung. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
