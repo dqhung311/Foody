95bc0d2d346198aa6c9de8638f8c76c4a5661309
Dao Quang Hung <daoquanghung@Daos-MacBook-Pro.local>
2017-05-23 09:41:18 +0700

    Commit structure project

---
+++ Foody/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foody
+//
+//  Created by Dao Quang Hung on 5/22/17.
+//  Copyright © 2017 Dao Quang Hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
