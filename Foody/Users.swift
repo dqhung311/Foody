952921e84baf646c95ccc25b6b31b8206c7e2933
Le NK <ngockhoale@gmail.com>
2017-05-31 21:26:54 +0700

    Bộ search mới

--- Foody/Users.swift
+++ Foody/Users.swift
@@ -22,6 +22,46 @@ class Users: NSObject {
         super.init()
     }
     
+    func insertUser() -> String{
+        var request = URLRequest(url: URL(string: "http://anphatkhanh.vn/foody/edit.php")!)
+        request.httpMethod = "POST"
+        let postString = "email=\(self.getEmail())&password=\(self.getPassword())&name=\(self.getName())"
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                // check for http errors
+                return httpStatus.statusCode
+//                DispatchQueue.main.async {
+//                    self.showAlertMessage("statusCode should be 200, but is \(httpStatus.statusCode)")
+//                }
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            let messageshow = responseString ?? ""
+            return messageshow
+//            if messageshow != "ok" {
+//                
+//                DispatchQueue.main.async {
+//                    self.showAlertMessage(messageshow)
+//                }
+//            } else {
+//                DispatchQueue.main.async {
+//                    UserDefaults.standard.setValue(newuser.getPassword(), forKey: "password")
+//                    self.showSuccessMessage("Đăng ký thành công")
+//                }
+//            }
+            
+        }
+        task.resume()
+        
+    }
+    
+    
+    
     func getEmail() -> String{
         return self.email
     }
--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@ target 'Foody' do
     
     
     pod 'AFNetworking', '~> 3.0'
+    pod 'Alamofire', '~> 4.4'
     
     # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
     use_frameworks!
