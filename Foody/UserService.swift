6cfbb622a212277eb7bf5941c12dee7f84e7ab25
Le NK <ngockhoale@gmail.com>
2017-06-04 20:43:43 +0700

    Sửa chỗ user để đổi code

--- Foody/UserService.swift
+++ Foody/UserService.swift
@@ -6,4 +6,56 @@
 //  Copyright © 2017 Dao Quang Hung. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class UserService {
+    let urlJson = "http://anphatkhanh.vn/foody/user/?"
+    
+    private let session : URLSession!
+    
+    init() {
+        session = URLSession(configuration: .default)
+    }
+    
+    func fetchUser(completion:  @escaping ([Users], NSError?) -> Void){
+        guard let url = URL(string: urlJson) else {
+            let error = NSError(domain: "UserService", code: 404, userInfo: [NSLocalizedDescriptionKey: "URL is invalid!"])
+            completion([], error)
+            return
+        }
+        
+        let task = session.dataTask(with: url, completionHandler: {[weak self] (data, res, err) in
+            guard let jsonData = data, let jsonObject = try? JSONSerialization.jsonObject(with: jsonData, options: []) else {
+                let error = NSError(domain: "UserService", code: 501, userInfo: [NSLocalizedDescriptionKey: "Response is invalid!"])
+                completion([], error)
+                return
+            }
+            
+            self?.parseJson(json: jsonObject as? [String: Any], completion: completion)
+        })
+        
+        task.resume()
+    }
+    
+    
+    func parseJson(json: [String: Any]?, completion: ([Users], NSError?) -> Void){
+        var userStore = [Users]()
+        if let user = json?["user"] as? [[String:Any]] {
+            for p in user{
+                let user = Users()
+                if let email = p["email"] as? String {
+                    user.email = email
+                }
+                if let password = p["password"] as? String {
+                    user.password = password
+                }
+                if let name = p["name"] as? String {
+                    user.name = name
+                }
+                userStore.append(user)
+            }
+        }
+        completion(userStore, nil)
+    }
+    
+}
--- Foody/Users.swift
+++ Foody/Users.swift
@@ -8,71 +8,20 @@
 
 import UIKit
 
-class Users: NSObject {
-    private var email: String
-    private var password: String
-    private var name: String
-    private var phone: String?
+class Users{
+    var email: String
+    var password: String
+    var name: String
+    
+    init() {
+        self.email = ""
+        self.password = ""
+        self.name = ""
+    }
     
     init(_ email: String, _ password: String, _ name: String) {
         self.email = email
         self.password = password
         self.name = name
-        
-        super.init()
-    }
-    
-    func insertUser() -> String{
-//        var request = URLRequest(url: URL(string: "http://anphatkhanh.vn/foody/user/edit.php")!)
-//        request.httpMethod = "POST"
-//        let postString = "email=\(self.getEmail())&password=\(self.getPassword())&name=\(self.getName())"
-//        request.httpBody = postString.data(using: .utf8)
-//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-//            guard let data = data, error == nil else {
-//                return
-//            }
-//            
-//            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
-//                // check for http errors
-//                return httpStatus.statusCode
-////                DispatchQueue.main.async {
-////                    self.showAlertMessage("statusCode should be 200, but is \(httpStatus.statusCode)")
-////                }
-//            }
-//            
-//            let responseString = String(data: data, encoding: .utf8)
-//            let messageshow = responseString ?? ""
-//            return messageshow
-////            if messageshow != "ok" {
-////                
-////                DispatchQueue.main.async {
-////                    self.showAlertMessage(messageshow)
-////                }
-////            } else {
-////                DispatchQueue.main.async {
-////                    UserDefaults.standard.setValue(newuser.getPassword(), forKey: "password")
-////                    self.showSuccessMessage("Đăng ký thành công")
-////                }
-////            }
-//            
-//        }
-//        task.resume()
-        return ""
-        
-    }
-    
-    
-    
-    func getEmail() -> String{
-        return self.email
-    }
-    
-    func getPassword() -> String{
-        return self.password
-    }
-    
-    func getName() -> String{
-        return self.name
-    }
-    
+    }    
 }
