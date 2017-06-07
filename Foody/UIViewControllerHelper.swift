45222975e23ed0a70f5848155caca200c17ebbe6
Le NK <ngockhoale@gmail.com>
2017-06-07 16:52:52 +0700

    Submit lại

--- Foody/UIViewControllerHelper.swift
+++ Foody/UIViewControllerHelper.swift
@@ -1,19 +1,19 @@
 //
-//  UIViewControllerHelper.swift
-//  Foody
+//  UIViewControllerHelper.swift
+//  Foody
 //
-//  Created by Le NK on 6/6/17.
-//  Copyright © 2017 Dao Quang Hung. All rights reserved.
+//  Created by Le NK on 6/6/17.
+//  Copyright © 2017 Dao Quang Hung. All rights reserved.
 //
 
 import UIKit
 
 extension UIViewController {
-    
+
     func dismissOne(){
         self.dismiss(animated: true, completion: nil)
     }
-    
+
     func dismissAll(){
         self.view.window!.rootViewController?.dismiss(animated: true, completion: nil)
     }
@@ -36,7 +36,7 @@ extension UIViewController {
         }
         return false
     }
-    
+
     func signOut(){
         UserDefaults.standard.removeObject(forKey: "password")
         UserDefaults.standard.removeObject(forKey: "email")
@@ -48,7 +48,6 @@ extension UIViewController {
         let alertController = UIAlertController(title: "Lỗi", message: "\(message)", preferredStyle: .alert)
         let defaultAction = UIAlertAction(title: "Đóng", style: .default, handler: nil)
         alertController.addAction(defaultAction)
-        
         present(alertController, animated: true, completion: nil)
     }
     
@@ -58,7 +57,31 @@ extension UIViewController {
             self.dismissAll()
         })
         alertController.addAction(defaultAction)
-        
         present(alertController, animated: true, completion: nil)
+    }
+    
+    func heightForView(text:String, font: UIFont, width: CGFloat) -> CGFloat{
+        let label:UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: CGFloat.greatestFiniteMagnitude))
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.font = font
+        label.text = text
+        label.sizeToFit()
+        return label.bounds.height
+    }
+    
+    func getLoginEmail() -> String{
+        return UserDefaults.standard.value(forKey: "email") as! String
+    }
+    
+    func getLoginName() -> String{
+        return UserDefaults.standard.value(forKey: "name") as! String
+    }
+    func getUserImageUrl(user: Users?) -> String{
+        if let user = user{
+            return (user.image_url)
+        }else{
+            return "http://www.iconsfind.com/wp-content/uploads/2015/08/20150831_55e46ad551392.png"
+        }
     }
 }
